072d2c52d0cbeb20cb0f6bf332fd9b983adadf0b
Aditya Raul <raul.aditya912@gmail.com>
2023-11-15 21:11:06 +0530

    Removed Layout & Contacts component

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Layout } from "./components/Layout";
 import { useContext } from "react";
 import { AuthContext } from "./contexts/AuthContext";
 // Pages
@@ -12,14 +11,12 @@ export const Router = () => {
   const { user } = useContext(AuthContext);
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={!user ? <Intro /> : <Navigate to="/home" />} />
-          <Route path="/home" element={user ? <Home /> : <Navigate to="/auth" />} />
-          <Route path="/auth" element={!user ? <Auth /> : <Navigate to="/home" />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route path="/" element={!user ? <Intro /> : <Navigate to="/home" />} />
+        <Route path="/home" element={user ? <Home /> : <Navigate to="/auth" />} />
+        <Route path="/auth" element={!user ? <Auth /> : <Navigate to="/home" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/components/home/Contacts.js
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-export const Contacts = () => {
-  return (
-    <Box sx={{ width: "40%", padding: 5 }}>
-      <Box sx={{ width: "100%", padding: 2 }}></Box>
-    </Box>
-  );
-};
