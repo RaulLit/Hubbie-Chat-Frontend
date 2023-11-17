080e39a3ad02533a92a98ebbcb1f2e2efe0f2300
Aditya Raul <raul.aditya912@gmail.com>
2023-11-17 11:08:41 +0530

    Integrated environment variables

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setError(null);
     setIsLoading(true);
 
-    const response = await fetch("http://localhost:4000/api/user/login", {
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/user/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
