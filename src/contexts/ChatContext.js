9fa3155a4e53f2cb3a1ea7ff8c94dcc7eb85f9ab
Aditya Raul <raul.aditya912@gmail.com>
2023-12-05 08:33:07 +0530

    initialised chat useState with []

--- src/contexts/ChatContext.js
+++ src/contexts/ChatContext.js
@@ -5,7 +5,7 @@ export const ChatContext = createContext();
 export const ChatContextProvider = ({ children }) => {
   const [selectedChat, setSelectedChat] = useState();
   const [notification, setNotification] = useState([]);
-  const [chats, setChats] = useState();
+  const [chats, setChats] = useState([]);
 
   return (
     <ChatContext.Provider
